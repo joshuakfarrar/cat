d42c84af2bc34d36796d93ec2448fca1ab21bc11
Joshua K. Farrar <joshua@sent1nel.me>
2015-07-07 22:05:02 -0400

    try buffering into file into character array to increase speed

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
 ### Benchmarks
 
-v0.1.0
+###### Time to beat:
 
 ```sh
-$ time zcat test.txt &>/dev/null
-
-real	0m28.657s
-user	0m26.120s
-sys	0m2.364s
-
 $ time cat test.txt &>/dev/null
 
-real	0m2.163s
-user	0m0.012s
-sys	0m0.392s
+real    0m2.163s
+user    0m0.012s
+sys 0m0.392s
+```
+
+###### My times
+
+v0.1.0
+
+> **Reads from fp char by char and fprintf's to stdout**
+
+```sh
+$ time ./cat ~/test.txt &>/dev/null
+
+real    0m28.657s
+user    0m26.120s
+sys 0m2.364s
+```
+
+v0.2.0
+
+> **Creates a buffer of length of file size, copies in file, and prints**
+
+```sh
+$ time ./cat ~/test.txt &>/dev/null
+
+real    0m3.117s
+user    0m0.172s
+sys 0m1.080s
 ```
--- cat.c
+++ cat.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int catFiles(int argc, char * argv[]) {
-    int i;
-    FILE * fp;
+int printStdin(int argc, char *argv[]) {
     int ch;
+
+    while((ch = fgetc(stdin)) != EOF) {
+       fprintf(stdout, "%c", ch);
+    }
+
+    return 0;
+}
+
+int getFileLength(FILE *fp) {
+    int length;
+
+    fseek(fp, 0L, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+
+    return length;
+}
+
+int catFiles(int argc, char *argv[]) {
+    int i;
+    FILE *fp;
+    int length;
+    char *buffer;
 
     for (i = 1; i < argc; i++) {
         fp = fopen(argv[i], "r");
         if (fp == NULL) {
             fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[i]);
+            exit(1);
         } else {
-            while((ch = fgetc(fp)) != EOF) {
-                fprintf(stdout, "%c", ch);
-            }
+            length = getFileLength(fp);
+            buffer = malloc(sizeof(char) * (length + 1));
+            fread(buffer, sizeof(char), length, fp);
+            fprintf(stdout, "%s", buffer);
             fclose(fp);
         }
     }
@@ -21,13 +45,9 @@ int catFiles(int argc, char * argv[]) {
     return 0;
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        int ch;
-        while((ch = fgetc(stdin)) != EOF) {
-            fprintf(stdout, "%c", ch);
-        }
-        return 0;
+        return printStdin(argc, argv);
     } else {
         return catFiles(argc, argv);
     }
